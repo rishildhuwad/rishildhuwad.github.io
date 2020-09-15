17314b888f38be1e6cf97eb14377368335677bda
rishildhuwad <68642326+rishildhuwad@users.noreply.github.com>
2020-09-15 22:18:00 +0530

    Update product_info.js

--- product_info.js
+++ product_info.js
@@ -5,8 +5,43 @@
  */
 
 $(document).ready(function(){
-  $("h3.htr").click(function(){
-    $("p.htr").slideToggle("slow");
+  $("#htr").click(function(){
+    $("#htr_info").slideToggle("slow");
   });
 });
 
+$(document).ready(function(){
+  $("#8000s").click(function(){
+    $("#8000s_info").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#6000s").click(function(){
+    $("#6000s_info").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#5000s").click(function(){
+    $("#5000s_info").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#1000s").click(function(){
+    $("#1000s_info").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#sc").click(function(){
+    $("#sc_info").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#2000s").click(function(){
+    $("#2000s_info").slideToggle("slow");
+  });
+});

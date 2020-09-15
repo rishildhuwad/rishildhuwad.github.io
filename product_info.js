89a749e833a9ef46542af899f72a94f60d0d7bc4
rishildhuwad <68642326+rishildhuwad@users.noreply.github.com>
2020-09-15 21:48:30 +0530

    Add files via upload

---
+++ product_info.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+  $("h3.htr").click(function(){
+    $("p.htr").slideToggle("slow");
+  });
+});
+

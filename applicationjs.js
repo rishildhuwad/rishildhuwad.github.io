81a00e5a6cc21d7e03de43cbae75d3cac1a271c4
rishildhuwad <68642326+rishildhuwad@users.noreply.github.com>
2020-11-13 13:47:44 +0530

    Update applicationjs.js

--- applicationjs.js
+++ applicationjs.js
@@ -40,3 +40,85 @@ function mOutGpaint()
 {
     document.getElementById("gpaint").innerHTML="";
 }
+
+
+function mOverGanpatipaint()
+{
+    document.getElementById("ganpatipaint").innerHTML="Recommended:<br/>8000 series,6000 series,1000 series";
+}
+function mOutGanpatipaint()
+{
+    document.getElementById("ganpatipaint").innerHTML="";
+}
+
+function mOverPaint()
+{
+    document.getElementById("paint").innerHTML="Recommended:<br/>8000 series,6000 series";
+}
+function mOutPaint()
+{
+    document.getElementById("paint").innerHTML="";
+}
+
+function mOverPapercoat()
+{
+    document.getElementById("papercoat").innerHTML="Recommended:<br/>8000 series,6000 series";
+}
+function mOutPapercoat()
+{
+    document.getElementById("papercoat").innerHTML="";
+}
+
+function mOverPlastic()
+{
+    document.getElementById("plastic").innerHTML="Recommended:<br/>8000 series,5000 series,HTR series";
+}
+function mOutPlastic()
+{
+    document.getElementById("plastic").innerHTML="";
+}
+
+function mOverPoster()
+{
+    document.getElementById("poster").innerHTML="Recommended:<br/>8000 series,6000 series";
+}
+function mOutPoster()
+{
+    document.getElementById("poster").innerHTML="";
+}
+
+function mOverPrintink()
+{
+    document.getElementById("printink").innerHTML="Recommended:<br/>8000 series,5000 series";
+}
+function mOutPrintink()
+{
+    document.getElementById("printink").innerHTML="";
+}
+
+function mOverRubber()
+{
+    document.getElementById("rubber").innerHTML="Recommended:<br/>8000 series,6000 series";
+}
+function mOutRubber()
+{
+    document.getElementById("rubber").innerHTML="";
+}
+
+function mOverTextile()
+{
+    document.getElementById("textile").innerHTML="Recommended:<br/>8000 series,6000 series";
+}
+function mOutTextile()
+{
+    document.getElementById("textile").innerHTML="";
+}
+
+function mOverWax()
+{
+    document.getElementById("wax").innerHTML="Recommended:<br/>8000 series,5000 series";
+}
+function mOutWax()
+{
+    document.getElementById("wax").innerHTML="";
+}
